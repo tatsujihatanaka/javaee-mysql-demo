44d78cb0ec38ea7272722405488872558bccbbe3
hatanaka <tatsuji.hatanaka@gmail.com>
2026-01-31 12:01:05 +0900

    Add HelloServlet and directory structure

---
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><body>");
+            out.println("<h1>Hello, Java EE 8 World!</h1>");
+            out.println("<p>Time: " + new java.util.Date() + "</p>");
+            out.println("</body></html>");
+        }
+    }
+}
+
