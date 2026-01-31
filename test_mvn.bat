2c8dc5cc98b29580f12c0c6e05c9b2e7552dfd34
hatanaka <tatsuji.hatanaka@gmail.com>
2026-01-31 15:57:33 +0900

    Fix boot script for test and reorganize test URLs for browser access

--- test_mvn.bat
+++ test_mvn.bat
@@ -8,8 +8,9 @@ for /f "usebackq tokens=*" %%i in (".env") do (
 
 rem 2. 読み込み確認（デバッグ用）
 echo --- Check Environment Variables ---
-echo DB_URL: %AI_SENSEI_DB_URL%
+echo DB_URL: "%AI_SENSEI_DB_URL%"
 echo DB_USER: %AI_SENSEI_DB_USER%
+echo AI_SENSEI_DB_PASS: %AI_SENSEI_DB_PASS%
 echo ---------------------------------
 
 rem 3. ビルドしてPayaraを起動
